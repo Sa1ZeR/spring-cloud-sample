d2eef14f671ba8b24a395be82393de865d81b87c
Serg1K <amsher2007@mail.ru>
2022-09-29 16:24:38 +1000

    init

---
+++ eureka-server/src/test/java/me/sa1zer/microsample/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sa1zer.microsample.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
