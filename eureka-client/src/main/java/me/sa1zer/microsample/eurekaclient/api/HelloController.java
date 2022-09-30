d45d94149e7503be214158f00855ef0821ef9fa1
Serg1K <amsher2007@mail.ru>
2022-09-30 16:36:55 +1000

    api gateway

--- eureka-client/src/main/java/me/sa1zer/microsample/eurekaclient/api/HelloController.java
+++ eureka-client/src/main/java/me/sa1zer/microsample/eurekaclient/api/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("main")
 public class HelloController {
 
     @GetMapping("/")
