282400d5a2fadbe6a497d124a679d527f2023255
Serg1K <amsher2007@mail.ru>
2022-10-02 14:27:07 +1000

    few instance support

--- eureka-client/src/main/java/me/sa1zer/microsample/eurekaclient/api/HelloController.java
+++ eureka-client/src/main/java/me/sa1zer/microsample/eurekaclient/api/HelloController.java
@@ -1,16 +1,20 @@
 package me.sa1zer.microsample.eurekaclient.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("main")
+@RequestMapping("/main/")
 public class HelloController {
+
+    @Value("${eureka.instance.instance-id}")
+    private String id;
 
     @GetMapping("/")
     public ResponseEntity<?> hello() {
-        return ResponseEntity.ok("hello");
+        return ResponseEntity.ok(String.format("hello from %s micro-service", id));
     }
 }
